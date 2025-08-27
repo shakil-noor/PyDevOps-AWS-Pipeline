a27ca6be4c13f0e1d9d574e1ab2d57190b914fdd
Nur Hossain Sakil <hossain.bd1182@gmail.com>
2025-08-28 01:55:20 +0300

    Add main variable file and provider=AWS

---
+++ jenkins-setup/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "DevOps Project 1 VPC 1"
+}
