aeeacf2f84a033f2d2418a3d422557ee0080836d
Nur Hossain Sakil <hossain.bd1182@gmail.com>
2025-08-29 02:11:05 +0300

    Add Internet Gateway and two route table, public and private. Associate these with the subnets accordingly

--- jenkins-setup/networking/main.tf
+++ jenkins-setup/networking/main.tf
@@ -48,3 +48,46 @@ resource "aws_subnet" "pydevops_proj_1_private_subnets" {
     Name = "pydevops-proj-private-subnet-${count.index + 1}"
   }
 }
+
+# Setup Internet Gateway
+resource "aws_internet_gateway" "pydevops_proj_1_public_internet_gateway" {
+  vpc_id = aws_vpc.pydevops_proj_1_vpc_us_east_1.id
+  tags = {
+    Name = "pydevops-proj-1-igw"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "pydevops_proj_1_public_route_table" {
+  vpc_id = aws_vpc.pydevops_proj_1_vpc_us_east_1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.pydevops_proj_1_public_internet_gateway.id
+  }
+  tags = {
+    Name = "pydevops-proj-1-public-rt"
+  }
+}
+
+# Public Route Table and Public Subnet Association
+resource "aws_route_table_association" "pydevops_proj_1_public_rt_subnet_association" {
+  count          = length(aws_subnet.pydevops_proj_1_public_subnets)
+  subnet_id      = aws_subnet.pydevops_proj_1_public_subnets[count.index].id
+  route_table_id = aws_route_table.pydevops_proj_1_public_route_table.id
+}
+
+# Private Route Table
+resource "aws_route_table" "pydevops_proj_1_private_route_table" {
+  vpc_id = aws_vpc.pydevops_proj_1_vpc_us_east_1.id
+  #depends_on = [aws_nat_gateway.nat_gateway]
+  tags = {
+    Name = "pydevops-proj-1-private-rt"
+  }
+}
+
+# Private Route Table and private Subnet Association
+resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
+  count          = length(aws_subnet.pydevops_proj_1_private_subnets)
+  subnet_id      = aws_subnet.pydevops_proj_1_private_subnets[count.index].id
+  route_table_id = aws_route_table.pydevops_proj_1_private_route_table.id
+}
